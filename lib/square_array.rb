abc14591fe414f61a2b2cb4bb1d02a055471db34
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 14:09:54 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+#Squares each element in an array
+#Returns a new array of these squared numbers
+
 def square_array(array)
-  # your code here
+  i=0
+  squares = []
+while i<array.length do
+  squares.push(array[i]**2)
+  i+=1
+end
+squares
 end
